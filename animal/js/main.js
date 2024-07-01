572c0c510a83d5c9c452a4fe9eca44c83bbe6e43
by91611 <by91611@gmail.com>
2024-07-01 14:11:27 +0900

    240701 동물보호센터

--- animal/js/main.js
+++ animal/js/main.js
@@ -1,4 +1,26 @@
 $(document).ready(function(){
+
+    /* 브라우저 넓이가  1024px 이하-mobile, 1024px 이상-pc*/ 
+    let pc_mobile // 현재 상태가 pc인지 mobile인지를 저장
+    let window_w // 브라우저 넓이 저장
+
+    function resize_chk(){
+        window_w = $(window).width()
+        if(window_w > 1024){ //1024보다 크면 - pc
+            pc_mobile = 'pc'
+        }else{ // mobile
+            pc_mobile = 'mobile'
+        }
+        console.log(pc_mobile)
+    }
+    // 처음에 로딩됐을 때 실행
+    resize_chk()
+    // 브라우저가 리사이즈 될 때마다 실행
+    $(window).resize(function(){
+        resize_chk()
+    })
+
+
     const visual_swiper = new Swiper('.visual .swiper', { 
         autoplay: {  
             delay: 2500,
@@ -47,5 +69,43 @@ $(document).ready(function(){
         /* 브라우저를 스크롤할 때마다 실행 */
         scroll_chk()
     }) //window.scroll
+
+    /* 메뉴에 마우스를 올리면 
+       header에 menu_over클래스 추가
+       마우스를 오버한 li에만 on이라는 클래스 추가 
+       메뉴: header .gnb .gnb_wrap ul.depth1 > li 
+       
+       -> 다른 li에 마우스를 오버하면 이전에 오버했던 li의 on클래스 삭제
+          (이전에 오버했던 메뉴를 알기 어려워서 모든 메뉴의 on클래스 삭제)
+       -> header에서 마우스를 아웃하면 menu_over클래스 삭제, 모든 메뉴에서 on 클래서 삭제 */ 
+
+    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function(){
+        if(pc_mobile == 'pc'){ //pc일 경우에만 실행
+            $('header').addClass('menu_over')
+            $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('on')
+            $(this).addClass('on') //mouse를 오버한 li만
+        }//if        
+    })//on
+    $('header').on('mouseleave', function(){
+        if(pc_mobile == 'pc'){ //pc일 경우에만 실행
+            $('header').removeClass('menu_over')
+            $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('on')
+        }//if
+    })//on
+
+    $('header .gnb .gnb_open').on('click', function(){
+        if(pc_mobile == 'mobile'){ //모바일일 경우에만 실행
+            $('header').addClass('mobile_open')
+        }//if
+    })
+    $('header .gnb .gnb_close').on('click', function(){
+        if(pc_mobile == 'mobile'){ //모바일일 경우에만 실행
+            $('header').removeClass('mobile_open')
+        }//if
+    })
+
+
+
+
 }) //document
 
