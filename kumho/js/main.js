28c43841f8516f88218fda9c51ffd61e948624b5
by91611 <by91611@gmail.com>
2024-07-18 14:11:13 +0900

    240718 금호건설

--- kumho/js/main.js
+++ kumho/js/main.js
@@ -35,4 +35,25 @@ $(document).ready(function(){
             $(this).hide()
             $('.visual .btn_wrap button.btn_stop').show()
         })
+
+
+        const news_swiper = new Swiper('.news .swiper', { /* 팝업을 감싼는 요소의 class명 */
+            slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+            spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+            breakpoints: {
+                768: {    /* 768px 이상일때 적용 */
+                slidesPerView: 2,
+                spaceBetween: 24,
+            },
+                1024: {    /* 768px 이상일때 적용 */
+                    slidesPerView: 3,
+                    spaceBetween: 24,
+                },
+            },
+            scrollbar: {
+                el: ".news .swiper-scrollbar",
+                hide: false,
+                draggable: true,
+              },
+    });
 })
