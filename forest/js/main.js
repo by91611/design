288f556287cce5fdfb9e05f393116a8fa6dff1d5
by91611 <by91611@gmail.com>
2024-08-26 13:17:30 +0900

    240826 생명의숲

--- forest/js/main.js
+++ forest/js/main.js
@@ -26,9 +26,12 @@ $(document).ready(function(){
 			console.log(destination.index)
 			$('.fp_nav ul li').removeClass('active')
 			$('.fp_nav ul li').eq(destination.index).addClass('active')
+			if(destination.index == 1){ //tree가 로딩되었을 때만 실행
+				$('.tree .count span').counterUp();
+			}
 		},
 
-		responsiveWidth: 1024 /* fullpage를 적용시키지 않을 모바일 사이즈 */
+		responsiveWidth: 1025 /* fullpage를 적용시키지 않을 모바일 사이즈 */
 	});
 
 	const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
@@ -48,4 +51,34 @@ $(document).ready(function(){
 			},
 		},
 	});	
+
+	const story_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+			640: {    /* 640px 이상일때 적용 */
+				slidesPerView: 4,
+				spaceBetween: 24,
+			},
+			768: {    /* 768px 이상일때 적용 */
+				slidesPerView: 4,
+				spaceBetween: 24,
+			},
+			1024: {   /* 1024px 이상일때 적용 */
+				slidesPerView: 4,
+				spaceBetween: 24,
+			},
+			1500: {    /* 1280px 이상일때 적용 */
+				slidesPerView: 4,
+				spaceBetween: 24,
+			},
+		},
+		// centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+		// autoplay: {  /* 팝업 자동 실행 */
+		// 	delay: 2500,
+		// 	disableOnInteraction: true,
+		// },
+	});
+
 })
