dd2776d0af411689260541b14e8b89a25569a75e
by91611 <by91611@gmail.com>
2024-09-02 17:59:40 +0900

    240902 생거진천치유의숲

--- healing/js/main.js
+++ healing/js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
 		pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-			el: '.visual .swiper-pagination', /* 해당 요소의 class명 */
+			el: '.visual .paging', /* 해당 요소의 class명 */
 			clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
 		},
 
@@ -23,9 +23,42 @@ $(document).ready(function(){
 		centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
 		loop: false,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 		navigation: {  /* 이전, 다음 버튼 */
-			nextEl: '.program .btn_next',  /* 다음 버튼의 클래스명 */
-			prevEl: '.program .btn_prev',  
+			nextEl: '.program .swiper-button-next',  /* 다음 버튼의 클래스명 */
+			prevEl: '.program .swiper-button-prev',  
 		},
 
 	});
+
+	const gallery_swiper_top = new Swiper('.gallery .swiper_top', { /* 팝업을 감싼는 요소의 class명 */
+
+		slidesPerView: "auto", /* li의 넓이 비율로 안함 - css에서 준 넓이대로 함 */
+		spaceBetween: 24, /* li와 li사이 - 제일 작은 여백 */
+		breakpoints: {
+			768: {  /* 640px 이상이 되면 적용 */
+				spaceBetween: 16, 
+			},
+			1024: {  /* 1024px 이상이 되면 적용 */
+				spaceBetween: 20,
+			},
+		},
+		centeredSlides: false, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+	});
+
+	const gallery_swiper_bottom = new Swiper('.gallery .swiper_bottom', { /* 팝업을 감싼는 요소의 class명 */
+
+		slidesPerView: "auto", /* li의 넓이 비율로 안함 - css에서 준 넓이대로 함 */
+		spaceBetween: 24, /* li와 li사이 - 제일 작은 여백 */
+		breakpoints: {
+			768: {  /* 640px 이상이 되면 적용 */
+				spaceBetween: 16, 
+			},
+			1024: {  /* 1024px 이상이 되면 적용 */
+				spaceBetween: 20,
+			},
+		},
+		centeredSlides: false, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+	});
+	
 })
