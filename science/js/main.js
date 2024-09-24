dfa16e0ee2909b41efc98b049586a0abf1b2715d
by91611 <by91611@gmail.com>
2024-09-24 18:00:47 +0900

    240924 한생연

--- science/js/main.js
+++ science/js/main.js
@@ -1,32 +1,56 @@
 $(document).ready(function(){
 
-	let program_active;
-	let program_list = $('.program .tab ul li')
-	let program_detail = '.program .tab_detail .detail_list .list'
-
-	program_list.on('click', function(){
-		program_list.attr('aria-selected', 'false')
-		$(this).attr('aria-selected', 'true')
-		program_active = $(this).attr('id')
-		
-		$(program_detail).removeClass('on')
-		$(program_detail + '[aria-labelledby="'+ program_active +'"]').addClass('on')
-		//.program .tab_detail .detail_list .list[aria-labelledby="tab_01"]
-    })
-
-	const notice_swiper = new Swiper('.notice .swiper', {
-		slidesPerView: "auto", 
-		spaceBetween: 16, 
+	const program_swiper = new Swiper('.program .swiper', { 
+		slidesPerView: 1, 
+		spaceBetween: 16,
 		breakpoints: {
-			1024: { 
-				slidesPerView: "auto",
-				spaceBetween: 24, 
+			768: {   
+				slidesPerView: 2,
+				spaceBetween: 16,
+			},
+			1024: {   
+				slidesPerView: 3,
+				spaceBetween: 20,
+			},
+			1300: { 
+				slidesPerView: 4,
+				spaceBetween: 24,
 			},
 		},
-		loop: true,
-		navigation: { 
-		nextEl: '.notice .btn_next',  
-		prevEl: '.notice .btn_prev',  
-	},
-	}); 
+		navigation: {
+			nextEl: '.program .next',
+			prevEl: '.program .prev',
+		},
+		scrollbar: {
+			el: ".program .swiper-scrollbar",
+			hide: false,
+			draggable: true,
+		},
+	});
+
+    const notice_swiper = new Swiper('.notice_right .swiper', { 
+
+        autoplay: { 
+            delay: 2500,
+            disableOnInteraction: true,
+        },
+
+        loop: true, 
+
+        pagination: {  
+            el: '.notice_right .btn_wrap .paging', 
+            clickable: true,  
+        },
+    });
+
+	$('.notice_right .btn_wrap button.btn_stop').on('click', function(){
+		notice_swiper.autoplay.stop(); 
+		$(this).hide()
+		$('.notice_right.btn_wrap button.btn_play').show()
+	})
+	$('.notice_right .btn_wrap button.btn_play').on('click', function(){
+		notice_swiper.autoplay.start();  
+		$(this).hide()
+		$('.notice_right .btn_wrap button.btn_stop').show()
+	})
 })
