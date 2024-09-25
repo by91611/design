380a93108d0e8b5c788af3105c7daa7fb66d974b
by91611 <by91611@gmail.com>
2024-09-25 16:32:11 +0900

    240925 한생연

--- science/js/common.js
+++ science/js/common.js
@@ -1,15 +1,4 @@
 $(document).ready(function(){
-    /*
-        1024이하는 mobile, 1025이상은 pc
-        
-        header .header_sub .gnb .gnb_wrap ul.depth1 > li
-        pc모드일 때 마우스를 오버하면 
-        1. header에 menu_over 클래스 추가
-        2. l차 메뉴에 over 클래스 추가
-
-        브라우저의 스크롤을 내리면 header에 fixed 클래스 추가
-        다시 최상위로 올라가면 fixed 클래스 삭제
-    */
 
     let scrolling
     let scroll_top //header를 고정을 시작할 높이값
@@ -69,13 +58,6 @@ $(document).ready(function(){
         $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('on')
     })
 
-    /* 
-        모바일 메뉴
-        header .gnb .gnb_wrap ul.depth1 > li > a를 클릭했을 때
-        1. 1차메뉴 a의 href값을 무력화하여 해당 페이지로 이동하지 않도록 
-        2. li에 open 클래스 추가
-        3. 열린 메뉴를 클릭하면 닫히고, 닫힌 메뉴를 클릭하면 열림 (동시에 여러개 메뉴 열림 가능)
-    */ 
 
     $('header .gnb .gnb_wrap ul.depth1 > li > a').on('click', function(e){
         if(pc_mobile == 'mobile'){ //모바일에서만
@@ -105,6 +87,12 @@ $(document).ready(function(){
     })
 
     $('footer .top').on('click', function(){
+        $('html, body').animate({
+            scrollTop : 0
+        }, 500)
+    })
+
+    $('.quick .top').on('click', function(){
         $('html, body').animate({
             scrollTop : 0
         }, 500)
--- science/js/main.js
+++ science/js/main.js
@@ -17,10 +17,6 @@ $(document).ready(function(){
 				spaceBetween: 24,
 			},
 		},
-		navigation: {
-			nextEl: '.program .next',
-			prevEl: '.program .prev',
-		},
 		scrollbar: {
 			el: ".program .swiper-scrollbar",
 			hide: false,
@@ -31,26 +27,44 @@ $(document).ready(function(){
     const notice_swiper = new Swiper('.notice_right .swiper', { 
 
         autoplay: { 
-            delay: 2500,
+            delay: 25000,
             disableOnInteraction: true,
         },
 
         loop: true, 
 
         pagination: {  
-            el: '.notice_right .btn_wrap .paging', 
+            el: '.notice_right .paging', 
             clickable: true,  
         },
     });
 
-	$('.notice_right .btn_wrap button.btn_stop').on('click', function(){
-		notice_swiper.autoplay.stop(); 
-		$(this).hide()
-		$('.notice_right.btn_wrap button.btn_play').show()
-	})
-	$('.notice_right .btn_wrap button.btn_play').on('click', function(){
-		notice_swiper.autoplay.start();  
-		$(this).hide()
-		$('.notice_right .btn_wrap button.btn_stop').show()
-	})
+	const youtube_swiper = new Swiper('.youtube .swiper', { 
+		slidesPerView: 1, 
+		spaceBetween: 16,
+		breakpoints: {
+			495: {   
+				slidesPerView: 2,
+				spaceBetween: 16,
+			},
+			768: {   
+				slidesPerView: 3,
+				spaceBetween: 16,
+			},
+			1024: {   
+				slidesPerView: 4,
+				spaceBetween: 20,
+			},
+			1300: { 
+				slidesPerView: 4,
+				spaceBetween: 24,
+			},
+		},
+		scrollbar: {
+			el: ".youtube .swiper-scrollbar",
+			hide: false,
+			draggable: true,
+		},
+	});
+
 })
